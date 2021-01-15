8e14053a6a054c06aa2ab4966735adc4eb47cbb8
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-01-15 12:58:14 +0100

    Add and modify some useful comments and change the let variables in const

--- Course_for_beginners_Youtube/myTscript.js
+++ Course_for_beginners_Youtube/myTscript.js
@@ -1,18 +1,18 @@
 // This three variables are the primary things you have to set up for every ThreeJS project:
 // 1. SCENE
-let scene = new THREE.Scene();
+const scene = new THREE.Scene();
 
 // 2. CAMERA
 // creating a PERSPECTIVE camera (there are also more) with 4 different parameters
-let camera = new THREE.PerspectiveCamera(
-  75, // field of view of the FOB (frontend or backend)
+const camera = new THREE.PerspectiveCamera(
+  75, // field of view (FOV = the extent of the scene that is seen on the display at any given moment. The value is in degrees)
   window.innerWidth/window.innerHeight, // the aspect ratio based on the inner width and inner height
    0.1, // near and far plane
   1000 // ???
   );
 
 // 3. RENDERER (WebGL, CSS 2d, CSS 3d, SVG)
-let renderer = new THREE.WebGLRenderer({antialias: true}); // otherwise the result will look jagged
+const renderer = new THREE.WebGLRenderer({antialias: true}); // otherwise the result will look jagged
 renderer.setClearColor("#e5e5e5"); // set the background color
 renderer.setSize(window.innerWidth,window.innerHeight); // set the size of the render
 
@@ -29,3 +29,20 @@ window.addEventListener('resize', () => {
 
 // in order to have the background colour set up we need to render it in the renderer with the method render
 renderer.render(scene, camera);
+
+// defining the geometry on our 3D element
+const geometry =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
