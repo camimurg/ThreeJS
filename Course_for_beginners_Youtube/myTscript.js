760d712927a876d1fd50e84eda4c0ba47a96cb18
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-01-30 21:46:16 +0100

    added some adjustment

--- Course_for_beginners_Youtube/myTscript.js
+++ Course_for_beginners_Youtube/myTscript.js
@@ -34,28 +34,37 @@ window.addEventListener('resize', () => {
   camera.updateProjectionMatrix();
 });
 
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
 // Defining the geometry on our 3D element
+
 // SphereGeometry(radius:Float, widthSegments:Int, heightSegments:Int)
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 // Creating the material
-const material = new THREE.MeshLambertMaterial( {color: 0xffcc00} );
-const mesh = new THREE.Mesh(geometry, material);
-
-mesh.position.set(2, 2, -2) // is the same as:
-// mesh.position.x = 2;
-// mesh.position.y = 2;
-// mesh.position.z = -2;
-
-// Other two properties that you can add:
-// mesh.rotation.set(45, 0, 0)
-// mesh.scale.set(1, 2, 1)
+const material = new THREE.MeshLambertMaterial({color: 0xffcc00});
+// const mesh = new THREE.Mesh(geometry, material);
 
 // We add our sphere that we've created above into our scene:
-scene.add(mesh);
+// scene.add(mesh);
+meshX = -10;
+for(var i = 0; i<15;i++) {
+  var mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = (Math.random() - 0.5) * 10;
+  mesh.position.y = (Math.random() - 0.5) * 10;
+  mesh.position.z = (Math.random() - 0.5) * 10;
+  scene.add(mesh);
+  meshX+=1;
+}
+
 
 // Adding light to our sphere
-const light = new THREE.PointLight(0xFFFFFF, 1, 500);
-light.position.set(10,0,25);
+var light = new THREE.PointLight(0xFFFFFF, 1, 1000);
+light.position.set(0,0,0);
+scene.add(light);
+
+var light = new THREE.PointLight(0xFFFFFF, 2, 1000);
+light.position.set(0,0,25);
 scene.add(light);
 
 // Creating a function where we set the animation property so that the object
@@ -70,22 +79,30 @@ const render = function() {
   renderer.render(scene, camera);
 };
 
+function onMouseMove(event) {
+  event.preventDefault();
+
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.x = - (event.clientY / window.innerHeight) * 2 + 1;
+
+  raycaster.setFromCamera(mouse, camera);
+
+  var intersects = raycaster.intersectObjects(scene.children, true);
+  for (var i = 0; i < intersects.length; i++) {
+    // intersects[i].object.material.color.set(colorname) --> to change the color
+
+    // this is linked with the pluggin on line 16 in the index.html
+    // !!! scale is not working, cannot read the property 'scale' of undefined!!!
+    this.tl = new TimelineMax();
+    this.tl.to(intersects[i].object.scale, 1, {x: 2, ease: Expo.easeOut});
+    this.tl.to(intersects[i].object.scale, .5, {x: .5, ease: Expo.easeOut});
+    this.tl.to(intersects[i].object.position, .5, {x: 2, ease: Expo.easeOut});
+    this.tl.to(intersects[i].object.rotation, .5, {y: Math.PI*.5, ease: Expo.easeOut}, "=-1.5");
+  }
+}
+
+window.addEventListener('mousemove', onMouseMove);
 render();
-
-// this is linked with the pluggin on line 16 in the index.html
-this.tl = new TimelineMax({paused: true});
-this.tl.to(this.mesh.scale, 1, {x: 2, ease: Expo.easeOut});
-this.tl.to(this.mesh.scale, .5, {x: .5, ease: Expo.easeOut});
-this.tl.to(this.mesh.position, .5, {x: 2, ease: Expo.easeOut});
-this.tl.to(this.mesh.rotation, .5, {y: Math.PI*.5, ease: Expo.easeOut});
-
-// we call an event listener in our body so that when the user click on it
-// the object will "play"
-document.body.addEventListener('click', () => {
-  this.tl.play();
-})
-
-
 
 
 
